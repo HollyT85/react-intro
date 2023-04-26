8727c6ab8838de46c215c19d6a41aef68e30c786
HollyT85 <h.e.turner@gmail.com>
2023-04-26 18:28:17 +0000

    conditional rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import EventBinding from './components/EventBinding';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from '.components/ConditionalRenderingFunctional';
+
 
 
 function App() {
   return (
     <div className="App">
-      <EventBinding />
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional />
     </div>
   );
 }
