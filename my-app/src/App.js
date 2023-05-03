fb0dc72b4a28b57f6cf690e135db2239eba7876a
HollyT85 <h.e.turner@gmail.com>
2023-05-03 17:38:54 +0000

    form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import LifeCyclesCWU from "./components/LifeCyclesCWU";
+import UncontrolledForm from "./components/UncontrolledForm";
 
 function App() {
   return <div className="App">
-      <LifeCyclesCWU />
+      <UncontrolledForm />
   </div>;
 }
 
